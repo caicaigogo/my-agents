7730d0b3cf5ce0461b02a3d8a47456139857373a
caicaigogo <cgl1991fzu@sina.com>
2026-02-11 13:33:49 +0800

    add max token test

--- tests/unit/test_core/test_llm.py
+++ tests/unit/test_core/test_llm.py
@@ -15,7 +15,16 @@ class TestHelloAgentsLLM(unittest.TestCase):
             'LLM_BASE_URL': os.getenv('LLM_BASE_URL'),
             'LLM_TIMEOUT': os.getenv('LLM_TIMEOUT')
         }
-        self.llm = HelloAgentsLLM()
+        use_third_party = False
+        if use_third_party:
+            self.llm = HelloAgentsLLM()
+        else:
+            provider = 'custom'
+            model = 'qwen2.5:0.5b'
+            public_host = os.getenv('PUBLIC_HOST')
+            base_url = "http://{}:11434/v1".format(public_host)
+            self.llm = HelloAgentsLLM(model=model, base_url=base_url, provider=provider)
+
         demo_messages = [
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": "你好"}
@@ -27,6 +36,19 @@ class TestHelloAgentsLLM(unittest.TestCase):
         response = self.llm.invoke(self.messages)
         print(response)
         self.assertIsInstance(response, str)
+
+    def test_max_token_invoke(self):
+        max_tokens = 5
+        response = self.llm._client.chat.completions.create(
+            model=self.llm.model,
+            messages=self.messages,
+            max_tokens=max_tokens
+        )
+        usage = response.usage
+        completion_tokens = usage.completion_tokens
+        # prompt_tokens = usage.prompt_tokens
+        # total_tokens = usage.total_tokens
+        self.assertEqual(completion_tokens, max_tokens)
 
     def test_stream_invoke(self):
 
