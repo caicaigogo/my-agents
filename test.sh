a9a7efefe34dfb6dbf4e1c44c320a69bacddc264
caicaigogo <cgl1991fzu@sina.com>
2026-03-04 11:13:37 +0800

    add test in test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 # python -m unittest -v tests.unit.test_memory.test_storage.test_document_store.TestSQLiteDocumentStore
 # python -m unittest -v tests.unit.test_memory.test_types.test_episodic.TestEpisodicMemory
 # python -m unittest -v tests.unit.test_memory.test_embedding.TestTFIDFEmbedding
-python -m unittest -v tests.unit.test_memory.test_storage.test_neo4j_store.TestNeo4jGraphStore
+# python -m unittest -v tests.unit.test_memory.test_storage.test_neo4j_store.TestNeo4jGraphStore
+ python -m unittest -v tests.unit.test_memory.test_types.test_semantic.TestSemanticMemory
