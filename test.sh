fa59f6560eb1f996510a60880810f5ff478840d8
caicaigogo <cgl1991fzu@sina.com>
2026-03-09 16:38:02 +0800

    fix name wrong

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@
 # python -m unittest -v tests.unit.test_memory.test_storage.test_neo4j_store.TestNeo4jGraphStore
 #python -m unittest -v tests.unit.test_memory.test_types.test_semantic.TestSemanticMemory
 #python -m unittest -v tests.unit.test_memory.test_types.test_semantic.TestSemanticMemory.test_retrieve
-python -m unittest -v tests.unit.test_memory.test_types.test_semantic.TestSemanticMemory.test_export_knowledge_graph
-python -m unittest -v tests.unit.test_protocols.test_mcp.TestSemanticMemory.test_client.TestMCPClient
+#python -m unittest -v tests.unit.test_memory.test_types.test_semantic.TestSemanticMemory.test_export_knowledge_graph
+python -m unittest -v tests.unit.test_protocols.test_mcp.test_client.TestMCPClient
