89bbad6434046cc2cc38d7535871c7551c44f6ab
caicaigogo <cgl1991fzu@sina.com>
2026-03-09 17:14:32 +0800

    add get github resource and prompts

--- tests/unit/test_protocols/test_mcp/test_client.py
+++ tests/unit/test_protocols/test_mcp/test_client.py
@@ -188,4 +188,23 @@ class TestMCPClient(unittest.IsolatedAsyncioTestCase):
 
         async with Client(server_source) as client:
             tools = await client.list_tools()
+            # 可用工具: ['add_comment_to_pending_review', 'add_issue_comment', 'add_reply_to_pull_request_comment',
+            #        'assign_copilot_to_issue', 'create_branch', 'create_or_update_file', 'create_pull_request',
+            #        'create_pull_request_with_copilot', 'create_repository', 'delete_file', 'fork_repository',
+            #        'get_commit', 'get_copilot_job_status', 'get_file_contents', 'get_label', 'get_latest_release',
+            #        'get_me', 'get_release_by_tag', 'get_tag', 'get_team_members', 'get_teams', 'issue_read',
+            #        'issue_write', 'list_branches', 'list_commits', 'list_issue_types', 'list_issues',
+            #        'list_pull_requests', 'list_releases', 'list_tags', 'merge_pull_request', 'pull_request_read',
+            #        'pull_request_review_write', 'push_files', 'request_copilot_review', 'search_code', 'search_issues',
+            #        'search_pull_requests', 'search_repositories', 'search_users', 'sub_issue_write',
+            #        'update_pull_request', 'update_pull_request_branch']
             print("可用工具:", [t.name for t in tools])
+
+
+            # 列出可用资源
+            resources =await client.list_resources()
+            print("resources", resources)
+
+            # 列出可用提示
+            prompts = await client.list_prompts()
+            print("prompts", prompts)
